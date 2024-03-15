58bc2f302d0c50275e09de5d3da2b1b97fe9f785
ItMeBayu <129031195+ItMeBayu@users.noreply.github.com>
2024-03-15 22:04:41 +0800

    Update dashboard.py

--- submission/dashboard.py
+++ submission/dashboard.py
@@ -67,7 +67,7 @@ def rfm_analysis_city(df):
 
     return rfm_df
 
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("submission/all_data.csv")
 
 datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
 all_df.sort_values(by="order_purchase_timestamp", inplace=True)
